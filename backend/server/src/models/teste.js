466905dbbe586f561c9abf956f8ae27e5ba00694
Manuel-Orlando <josemanuelorlandosilva@gmail.com>
2025-08-08 17:10:44 -0300

    refactor: melhorando a estrutura de pastas

--- backend/server/src/models/teste.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const TesteSchema = new mongoose.Schema({
-  nome: String,
-  ativo: Boolean,
-});
-
-module.exports = mongoose.model("Teste", TesteSchema);
-// backend/server/src/models/teste.js
--- backend/server/src/server.js
+++ backend/server/src/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./db");
+const connectDB = require("./config/database");
 const cors = require("cors");
 
 const app = express();
@@ -9,19 +9,6 @@ connectDB();
 // ✅ Middlewares primeiro
 app.use(cors());
 app.use(express.json());
-
-// ✅ Model
-const Teste = require("./models/teste");
-
-// ✅ Rota de teste
-app.post("/teste", async (req, res) => {
-  try {
-    const novo = await Teste.create({ nome: "Manuel", ativo: true });
-    res.json(novo);
-  } catch (err) {
-    res.status(500).json({ erro: "Erro ao salvar" });
-  }
-});
 
 // ✅ Rota padrão
 app.get("/", (req, res) => {
